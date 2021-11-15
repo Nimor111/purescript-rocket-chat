64d7e48556110ec1d03af1017b8b6f6722aef936
Nimor111 <georgi.bojinov@hotmail.com>
2021-11-16 00:23:29 +0200

    Move api url to a .env variable

--- src/Main.purs
+++ src/Main.purs
@@ -42,9 +42,6 @@ type PrivateGroupsResponse = {
   }
 }
 
-apiUrl :: String
-apiUrl = "https://wiki.gbojinov.xyz/api/v1"
-
 main :: Effect Unit
 main = do
   launchAff_
@@ -52,39 +49,43 @@ main = do
       _ <- Dotenv.loadFile
       username <- liftEffect $ lookupEnv "ROCKET_USERNAME"
       password <- liftEffect $ lookupEnv "ROCKET_PASSWORD"
-      res <- post RF.json (apiUrl <> "/login") (Just (RB.json (encodeJson $ { username, password })))
-      case res of
-        Left err -> do
-          log $ "GET /api response failed to decode: " <> printError err
-        Right response -> do
-          case (decodeJson response.body) of
-            Right (apiCreds :: ApiCredentials) -> do
-              let token = apiCreds.data.authToken
-              let userId = apiCreds.data.userId
-              log $ "Auth token is: " <> show token
-              log $ "User id is: " <> show userId
+      maybeApiUrl   <- liftEffect $ lookupEnv "ROCKET_API_URL"
+      case maybeApiUrl of
+        Nothing -> log "No API Url provided"
+        Just apiUrl -> do
+          res <- post RF.json (apiUrl <> "/login") (Just (RB.json (encodeJson $ { username, password })))
+          case res of
+            Left err -> do
+              log $ "GET /api response failed to decode: " <> printError err
+            Right response -> do
+              case (decodeJson response.body) of
+                Right (apiCreds :: ApiCredentials) -> do
+                  let token = apiCreds.data.authToken
+                  let userId = apiCreds.data.userId
+                  log $ "Auth token is: " <> show token
+                  log $ "User id is: " <> show userId
 
-              let req = defaultRequest {
-                --url = apiUrl <> "/channels.list.joined"
-                url = apiUrl <> "/groups.list"
-              , method = Left GET
-              , responseFormat = RF.json
-              , headers = [
-                  RequestHeader "X-Auth-Token" token
-                , RequestHeader "X-User-Id" userId
-                ]
-              }
+                  let req = defaultRequest {
+                    --url = apiUrl <> "/channels.list.joined"
+                    url = apiUrl <> "/groups.list"
+                  , method = Left GET
+                  , responseFormat = RF.json
+                  , headers = [
+                      RequestHeader "X-Auth-Token" token
+                    , RequestHeader "X-User-Id" userId
+                    ]
+                  }
 
-              resp <- request req
+                  resp <- request req
 
-              case resp of
+                  case resp of
+                    Left e -> do
+                      log $ "Can't parse JSON for group req. " <> printError e
+                    Right response -> do
+                      case (decodeJson response.body) of
+                        Left e ->
+                          log $ "Can't decode JSON" <> show e
+                        Right (r :: PrivateGroupsResponse) -> do
+                          log $ show r
                 Left e -> do
-                  log $ "Can't parse JSON for group req. " <> printError e
-                Right response -> do
-                  case (decodeJson response.body) of
-                    Left e ->
-                      log $ "Can't decode JSON" <> show e
-                    Right (r :: PrivateGroupsResponse) -> do
-                      log $ show r
-            Left e -> do
-              log $ "Can't parse JSON for auth req. " <> show e
+                  log $ "Can't parse JSON for auth req. " <> show e
